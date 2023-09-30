0b41b06da16cfc94e77957a641f3ddea211ab081
Andrew Jeffery <dev@jeffas.io>
2023-09-30 14:40:49 +0100

    Add flake and nixosModule

---
+++ flake.nix
@@ -0,0 +1,58 @@
+{
+  description = "Speedtest exporter";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+      python = pkgs.python3.withPackages (ps: with ps; [prometheus-client flask waitress]);
+    in {
+      packages.speedtest-exporter = pkgs.writeShellScriptBin "speedtest-exporter" ''
+        export PATH=${pkgs.ookla-speedtest}/bin:$PATH
+        ${python}/bin/python ${./src/exporter.py}
+      '';
+
+      devShells.default = pkgs.mkShellNoCC {
+        packages = [python];
+      };
+    })
+    // {
+      nixosModules.speedtest-exporter = {
+        lib,
+        pkgs,
+        config,
+        ...
+      }:
+        with lib; let
+          cfg = config.services.prometheus.exporters.speedtest;
+        in {
+          options = {
+            services.prometheus.exporters.speedtest = {
+              enable = mkEnableOption "speedest-exporter";
+              port = 9798;
+            };
+          };
+          config = mkIf cfg.enable {
+            systemd.services.prometheus-speedtest-exporter = {
+              wantedBy = ["multi-user.target"];
+              after = ["network.target"];
+              serviceConfig = {
+                ExecStart = "${self.packages.${pkgs.system}.speedtest-exporter}";
+                Environment = "SPEEDTEST_PORT=${toString cfg.port}";
+              };
+            };
+          };
+        };
+    };
+}
